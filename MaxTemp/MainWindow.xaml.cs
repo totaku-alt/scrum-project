de640b4aece48c2e44d633d047ae8efeadd96b2f
Tobias <tobiasheinen@live.com>
2022-10-27 09:02:34 +0200

    Update MainWindow.xaml.cs

--- MaxTemp/MainWindow.xaml.cs
+++ MaxTemp/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
@@ -18,15 +18,12 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
 
-namespace MaxTemp
-{
+namespace MaxTemp {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
 
@@ -37,40 +34,42 @@ namespace MaxTemp
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void BtnAuswerten_Click(object sender, RoutedEventArgs e)
-        {
+        private void BtnAuswerten_Click(object sender, RoutedEventArgs e) {
             //Zugriff auf Datei erstellen.
             String tempCSV = "C:\\Users\\Tobi\\Documents\\Schule\\Softwareentwicklung\\TempMaxProjekt\\MaxTemp(1)\\MaxTemp\\MaxTemp\\temps.csv";
-            List<string> csvList = new List<string>();
+
+            List<string> csvAsList = new List<string>();
+
             if (File.Exists(tempCSV)) {
                 StreamReader reader = new StreamReader(File.OpenRead(tempCSV));
                 while (!reader.EndOfStream) {
                     var line = reader.ReadLine();
                     var values = line.Split(',');
-                    foreach (var elements in values) {
-                        csvList.Add(elements);
+                    foreach (var elements in values) { 
                         Debug.WriteLine(elements);
+                        csvAsList.Add(elements);
                     }
                 }
 
             } else {
                 Debug.WriteLine("File not found");
             }
-            csvList.Sort();
+            csvAsList.Sort();
 
             //Anfangswert setzen, um sinnvoll vergleichen zu können.
 
             //In einer Schleife die Werte holen und auswerten. Den größten Wert "merken".
-            List<string> sortedList = csvList.GetRange(35, 33);
+            List<string> sortedList = csvAsList.GetRange(35, 33);
             foreach (var element in sortedList) {
                 Console.WriteLine(element);
             }
             String highestTemp = sortedList.Last();
-            //Datei wieder freigeben.
+            //Datei wieder freigeben
 
             //Höchstwert auf Oberfläche ausgeben.
+            lblAusgabe.Content = $"Höchste Temperatur ist: {highestTemp} °C";
 
-            MessageBox.Show("Gleich kachelt das Programm...");
+            //MessageBox.Show("Gleich kachelt das Programm...");
             //kommentieren Sie die Exception aus.
             //throw new Exception("peng");
         }
